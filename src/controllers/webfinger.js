8c349ede11c5b0aa89da44437ed7de3670fc63ab
Jonathan Hardison <jmh@jonathanhardison.com>
2022-12-21 01:19:55 -0600

    stub example webfinger

--- src/controllers/webfinger.js
+++ src/controllers/webfinger.js
@@ -12,7 +12,31 @@ module.exports = ({config}) => {
     
     // general
     api.get('/', (req, res) => {
-        res.json({status: "running"}).status(200);
+        res.type("application/jrd+json").json({
+            subject:"acct:jhardison@mastodon.social",
+            aliases:
+            [
+                "https://mastodon.social/@jhardison",
+                "https://mastodon.social/users/jhardison"
+            ],
+            links:
+            [
+                {
+                    rel:"http://webfinger.net/rel/profile-page",
+                    type:"text/html",
+                    href:"https://mastodon.social/@jhardison"
+                },
+                {
+                    rel:"self",
+                    type:"application/activity+json",
+                    href:"https://mastodon.social/users/jhardison"
+                },
+                {
+                    rel:"http://ostatus.org/schema/1.0/subscribe",
+                    template:"https://mastodon.social/authorize_interaction?uri={uri}"
+                }
+            ]
+        }).status(200);
     });
     
     return api;
